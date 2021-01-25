65d46ac33308b4b81878dcf37acf81b83d083bd3
Aikerim <aykerimm26@gmail.com>
2021-01-25 20:52:57 +0600

    added unmark-todo in test.html and function in views.py

--- main/views.py
+++ main/views.py
@@ -50,3 +50,15 @@ def delete_todo(request, id):
     todo.delete()
     return redirect(test)
 
+def mark_todo(request, id):
+    todo = ToDo.objects.get(id=id)
+    todo.is_favorite = True
+    todo.save()
+    return redirect(test)
+
+def unmark_todo(request, id):
+    todo = ToDo.objects.get(id=id)
+    todo.is_favorite = False
+    todo.save()
+    return redirect(test)
+
--- todo/urls.py
+++ todo/urls.py
@@ -27,6 +27,8 @@ urlpatterns = [
     path("test3", third),
     path("add-todo/", add_todo, name="add-todo"),
     path("delete-todo/<id>", delete_todo, name="delete-todo"),
+    path("mark-todo/<id>", mark_todo, name="mark-todo"),
+    path("unmark-todo/<id>", unmark_todo, name="unmark-todo"),
 
     path("books/", books, name="books"),
     path("add-book", add_book, name="add-book"),
